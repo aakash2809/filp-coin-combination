83df4d9936f8aa7062f5a65d64ba654c7321ffad
aakash2809 <aakashrajak289@gmail.com>
2020-10-31 14:15:46 +0530

    singlet wining percentage

--- FlipCoinCombinatio.sh
+++ FlipCoinCombinatio.sh
@@ -1,12 +1,37 @@
+
 #!/bin/bash -x
+
+headCount=0
+tailCount=0
+HEAD=0
+#TAIL=1
+count=0
+#DECLARETION OF DICTIONARY
+declare -A singlet
+
 
 echo "                       Welcome to fliping coin combination problem                   "
 echo "--------------------------------------------------------------------------------------"
+read -p "enter the no.of flips" numberOfFlips
+# STORE HEAD COUNT AND TAIL COUNT IN DICTIONARY
+for(( count=0 ; count<$numberOfFlips; count++ ))
+do
+   result=$(( RANDOM % 2 ))
 
-result=$((RANDOM%2));
-if [ $result -eq 0 ]
-then
-      echo "Heads"
-  else
-      echo "Tails"
-fi
+   if [ $result -eq $HEAD ]
+   then
+      singlet[HEAD]=$((++headCount))
+   else
+      singlet[TAIL]=$((++tailCount))
+   fi
+done
+
+#PERCENTAGE OF WININGS
+singletHeadPercentage=`echo "${singlet[HEAD]}" $numberOfFlips | awk '{ printf $1 * 100 / $2 }'`
+singletTailPercentage=`echo "${singlet[TAIL]}" $numberOfFlips | awk '{print $1 *100 / $2 }'`
+
+echo "head count:" $headCount
+echo "tail count:"$tailCount 
+echo "single head percentage : " $singletHeadPercentage "%"
+echo "single tail percentage : " $singletTailPercentage "%"
+
