bf80b779b11e728a424ff63066a7a08d94820237
aakash2809 <aakashrajak289@gmail.com>
2020-10-31 06:16:58 +0530

    Add : random win result

--- FlipCoinCombinatio.sh
+++ FlipCoinCombinatio.sh
@@ -1,4 +1,12 @@
 #!/bin/bash -x
 
-echo "                           Welcome to fliping coin combination problem"
-echo "----------------------------------------------------------------------------------"
+echo "                       Welcome to fliping coin combination problem                   "
+echo "--------------------------------------------------------------------------------------"
+
+result=$((RANDOM%2));
+if [ $result -eq 0 ]
+then
+      echo "Heads"
+  else
+      echo "Tails"
+fi
